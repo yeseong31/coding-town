39de8ad9eebd68a1a7c9470b16adf057ec0ae534
yeseong31 <yeseong31@naver.com>
2022-11-29 21:33:31 +0900

    feat: logging 적용

--- config/production.py
+++ config/production.py
@@ -1,3 +1,5 @@
+from logging.config import dictConfig
+
 from dotenv import load_dotenv
 
 from config.default import *
@@ -5,3 +7,27 @@ from config.default import *
 load_dotenv(verbose=True)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+
+# https://wikidocs.net/81081
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
