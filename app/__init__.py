78732349672c283a911013e2b2e99c69d1a01056
yeseong31 <yeseong31@naver.com>
2022-12-01 13:39:49 +0900

    test: socket.io test

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, session, render_template
+from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flask_restx import Api
-from flask_socketio import SocketIO, emit, join_room
+from flask_socketio import SocketIO, join_room, send, leave_room, emit
 
 users_in_room = {}  # 사용자
 rooms_sid = {}  # 방
@@ -38,104 +38,10 @@ def create_app():
     api.add_namespace(test, '/test')
     
     # --- Web RTC ---
-    socketio = SocketIO(app, cors_allowed_origins="*")
+    sio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
-    # @app.route('/test', methods=('GET',))
-    # def test():
-    #     display_name = request.args.get('display_name')  # 영상통화 사용자
-    #     mute_audio = request.args.get('mute_audio')  # 1 or 0: 오디오 on/off
-    #     mute_video = request.args.get('mute_video')  # 1 or 0: 영상 on/off
-    #     room_id = request.args.get('room_id')
-    #
-    #     # 세션은 room_id를 키로 가짐
-    #     session[room_id] = {'name': display_name,
-    #                         'mute_audio': mute_audio,
-    #                         'mute_video': mute_video}
-    #
-    #     return render_template('join.html',
-    #                            room_id=room_id,
-    #                            display_name=session[room_id]['name'],
-    #                            mute_audio=session[room_id]['mute_audio'],
-    #                            mute_video=session[room_id]['mute_video'])
-
-    @socketio.on('connect')
-    def on_connect():
-        emit("response", {'data': 'Connected', 'username': session['username']})
-        # print(f'New socket connected: {request.sid}')
-
-    @socketio.on('join-room')
-    def on_join_room(data):
-        sid = request.sid
-        room_id = data['room_id']
-        display_name = session[room_id]['name']
-    
-        # register sid to the room
-        join_room(room_id)
-        rooms_sid[sid] = room_id
-        names_sid[sid] = display_name
-    
-        # broadcast to others in the room
-        print(f'[{room_id}] New member joined: {display_name}<{sid}>')
-        emit('user-connect',
-             {'sid': sid, 'name': display_name},
-             broadcast=True,
-             include_self=False,
-             room=room_id)
-    
-        # add to user list maintained on server
-        if room_id not in users_in_room:
-            users_in_room[room_id] = [sid]
-            emit('user-list', {'my_id': sid})  # send own id only
-        else:
-            user_list = {uid: names_sid[uid] for uid in users_in_room[room_id]}
-            # send list of existing users to the new member
-            emit('user-list', {'list': user_list, 'my_id': sid})
-            # add new member to user list maintained on server
-            users_in_room[room_id].append(sid)
-    
-        print(f'\nusers: {users_in_room}\n')
-
-    @socketio.on('disconnect')
-    def on_disconnect():
-        sid = request.sid
-        room_id = rooms_sid[sid]
-        display_name = names_sid[sid]
-    
-        print(f'[{room_id}] Member left: {display_name}<{sid}>')
-        emit('user-disconnect',
-             {'sid': sid},
-             broadcast=True,
-             include_self=False,
-             room=room_id)
-    
-        users_in_room[room_id].remove(sid)
-        if len(users_in_room[room_id]) == 0:
-            users_in_room.pop(room_id)
-    
-        rooms_sid.pop(sid)
-        names_sid.pop(sid)
-    
-        print(f'\nusers: {users_in_room}\n')
-
-    @socketio.on('data')
-    def on_data(data):
-        sender_sid = data['sender_id']
-        target_sid = data['target_id']
-    
-        if sender_sid != request.sid:
-            print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
-    
-        if data['type'] != 'new-ice-candidate':
-            print(f'{data["type"]} message from {sender_sid} to {target_sid}')
-    
-        socketio.emit('data', data, room=target_sid)
-
+    @sio.on('connect')
     def message_received(methods=('GET', 'POST')):
-        print('message was received!!!')
-
-    @socketio.on('my event')
-    def handle_my_custom_event(json, methods=('GET', 'POST')):
-        print('received my event: ' + str(json))
-        socketio.emit('my response', json, callback=message_received)
+        return make_response(jsonify({'message': '메시지 받았음!'}), 200)
     
     return app
