a2eb2b3bfacc3d37652b012290a78364a7a9fe47
yeseong31 <yeseong31@naver.com>
2022-12-15 14:10:24 +0900

    feat: Lobby 전체 방 정보 조회

--- app/views/lobby_views.py
+++ app/views/lobby_views.py
@@ -1,6 +1,8 @@
 from flask import make_response, jsonify, request
 from flask_restx import Namespace, Resource
 
+from app.models import Room
+
 ns = Namespace(
     name="Lobby",
     description="Coding Town Lobby API",
@@ -26,7 +28,6 @@ class Lobby(Resource):
             - roomCode: 방에 부여된 고유한 6자리 랜덤 번호
             - tags: 방에 설정된 태그
             - isPrivate: 비밀번호가 걸린 방인지 확인
-            - password: 방 비밀번호 (isPrivate==False라면 빈 문자열)
             - currentUser: 방 참여자 수
             - totalUser: 방에 참여 가능한 참여자 수
         """
@@ -35,21 +36,25 @@ class Lobby(Resource):
 
         # 검색어
         search = request.args.get('search')
-        if search is None:
-            # 전체 정보 조회
+        if search is not None:
+            # search에 해당하는 방 정보 조회
             pass
 
-        # search에 해당하는 방 정보 조회
-        response_data = {
-            'roomCount': 0,
-            'rooms': {
-                'roomName': 'test',
-                'roomCode': 000000,
-                'tags': ['tags'],
-                'isPrivate': True,
-                'password': 'test',
-                'currentUser': 1,
-                'totalUser': 5
+        # 전체 방 정보 조회
+        rooms = []
+        for room in Room.query.order_by(Room.created_at).all():
+            data = {
+                'roomName': room.room_name,
+                'roomCode': room.room_code,
+                'tags': ['test', 'coding-town', 'tags'],
+                'isPrivate': room.is_private,
+                'currentUser': room.current_user,
+                'totalUser': room.total_user
             }
+            rooms.append(data)
+
+        response_data = {
+            'roomCount': len(rooms),
+            'rooms': rooms
         }
         return make_response(jsonify(response_data))
