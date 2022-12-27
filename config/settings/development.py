a4ca2ef1d85c9b787f4acd27766bf146e7efa01a
yeseong31 <yeseong31@naver.com>
2022-12-28 00:04:47 +0900

    feat: 개발 환경 DB 설정 추가

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,5 @@
+from dj_database_url import config
+
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -8,13 +10,19 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv('LOCAL_DB_ENGINE'),
+        'NAME': os.getenv('LOCAL_DB_NAME'),
+        'USER': os.getenv('LOCAL_DB_USER'),
+        'PASSWORD': os.getenv('LOCAL_DB_PASSWORD'),
+        'HOST': os.getenv('LOCAL_DB_HOST'),
+        'PORT': int(os.getenv('LOCAL_DB_PORT')),
     }
 }
+
+db_from_env = config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
