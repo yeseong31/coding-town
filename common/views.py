1729197406fcb8718bfda283a5682180d177c306
yeseong31 <yeseong31@naver.com>
2022-12-28 17:30:15 +0900

    feat: 회원 등록 및 조회 views 추가

--- common/views.py
+++ common/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+from common.models import MyUser as User
+from common.serializers import RegisterSerializer, SigninSerializer
+from rest_framework import generics, status
+
+
+class RegisterView(generics.CreateAPIView):
+    """사용자 등록: POST"""
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class SigninView(generics.GenericAPIView):
+    """사용자 조회 및 로그인: POST"""
+    serializer_class = SigninSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
