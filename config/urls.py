3fde2abe8854f080d0228a356e16e4c712e63df2
yeseong31 <yeseong31@naver.com>
2022-12-28 17:29:56 +0900

    feat: 회원 등록 및 조회 url 추가

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('common/', include('common.urls')),
     path('', include('sockets.urls')),
 ]
 
