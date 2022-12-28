b501c6eee6eba9ccf034a1e50b50fa81b3ef2e67
yeseong31 <yeseong31@naver.com>
2022-12-28 13:11:54 +0900

    feat: Custom User admin 설정

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from common.forms import UserAdmin
+from common.models import MyUser
+
+admin.site.register(MyUser, UserAdmin)
+admin.site.unregister(Group)
