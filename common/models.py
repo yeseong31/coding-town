ea7b5a2e9b1ffe08ef25de07f8582aff7b53051c
yeseong31 <yeseong31@naver.com>
2022-12-28 13:24:58 +0900

    rename: MyUser 모델 이름 변경
    db 표시명을 user로 변경

--- common/models.py
+++ common/models.py
@@ -65,3 +65,6 @@ class MyUser(AbstractBaseUser):
         """Is the user a member of staff?"""
         # Simplest possible answer: All admins are staff
         return self.is_admin
+
+    class Meta:
+        db_table = 'user'
