ac045627cfc3c99fdc0248bfb15bfb4cdb74f579
yeseong31 <yeseong31@naver.com>
2022-12-28 18:40:56 +0900

    feat: Room 모델 추가

--- sockets/models.py
+++ sockets/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from common.models import MyUser as User
+
+
+class Room(models.Model):
+    id = models.IntegerField(primary_key=True, verbose_name='room 인덱스')
+    sid = models.CharField(max_length=128, unique=True, verbose_name='room socketio id')
+    name = models.CharField(max_length=50, verbose_name='room 이름')
+    code = models.CharField(unique=True, verbose_name='room 코드')
+    is_private = models.BooleanField(default=False, verbose_name='암호화 여부')
+    password = models.CharField(max_length=128, null=True, blank=True, verbose_name='room 비밀번호')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms', verbose_name='room 생성자')
+    current_num = models.IntegerField(verbose_name='현재 참가 인원 수')
+    total_num = models.IntegerField(default=10, verbose_name='room 제한 인원 수')
+    create_at = models.DateTimeField(default=timezone.now, verbose_name='room 생성일')
+    modified_at = models.DateTimeField(null=True, blank=True, verbose_name='room 수정일')
+
+    def __str__(self):
+        return f'[{self.code}] {self.name}'
+
+    class Meta:
+        db_table = 'room'
