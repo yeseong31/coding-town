ea9afc5f5a65d054b036824822c75a39d7836267
yeseong31 <yeseong31@naver.com>
2022-12-30 00:28:58 +0900

    feat: Tag 모델 생성

--- sockets/models.py
+++ sockets/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='tag 이름')
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'tag'
+
+
 class Room(models.Model):
     sid = models.CharField(max_length=128, null=True, blank=True, verbose_name='room socketio id')
     name = models.CharField(max_length=50, verbose_name='room 이름')
@@ -13,6 +24,7 @@ class Room(models.Model):
     total_num = models.IntegerField(default=10, verbose_name='room 제한 인원 수')
     create_at = models.DateTimeField(default=timezone.now, verbose_name='room 생성일')
     modified_at = models.DateTimeField(null=True, blank=True, verbose_name='room 수정일')
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.code}] {self.name}'
