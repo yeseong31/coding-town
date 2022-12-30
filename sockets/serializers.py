39e354b57e6efdd364ed48135fb813352ccf1e52
yeseong31 <yeseong31@naver.com>
2022-12-30 11:25:57 +0900

    chore: /lobby tags 조회 내용 수정
    tag id 리스트가 아닌 name 리스트를 반환하도록 수정

--- sockets/serializers.py
+++ sockets/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from .models import Room
+from .models import Room, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    def to_representation(self, value):
+        return value.name
+    
+    class Meta:
+        model = Tag
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -9,7 +17,10 @@ class RoomSerializer(serializers.ModelSerializer):
     isPrivate = serializers.BooleanField(source='is_private')
     currentUser = serializers.IntegerField(source='current_num')
     totalUser = serializers.IntegerField(source='total_num')
-
+    tags = TagSerializer(read_only=True, many=True)
+    
     class Meta:
         model = Room
-        fields = ('roomName', 'roomCode', 'isPrivate', 'currentUser', 'totalUser', 'tags')
+        fields = ('roomName', 'roomCode', 'isPrivate', 'currentUser', 'totalUser', 'tags',)
+        extra_kwargs = {'roomCode': {'write_only': True}}
+        depth = 1
