03e0c7ad0158023ba9c561c0a200f7d893b920c2
yeseong31 <yeseong31@naver.com>
2023-01-17 17:25:15 +0900

    feat: 서버 DNS NAME 설정

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ELASTIC_IP')]
+ALLOWED_HOSTS = [os.getenv('ELASTIC_IP'), os.getenv('DNS_NAME')]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
